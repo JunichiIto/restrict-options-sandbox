3a18ae596157563610542dae256370ad5f8b4da4
Junichi Ito <jit@sonicgarden.jp>
2015-07-31 08:16:39 +0900

    Refactoring tests.

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -6,9 +6,19 @@ class CategoryTest < ActiveSupport::TestCase
     item = category.items.create!(name: 'Ruby入門')
     order = item.orders.create!(customer: 'Alice')
 
+    # 子レコードがあると削除できない
     assert_nothing_raised { item.destroy }
     assert_equal ["Cannot delete record because dependent orders exist"], item.errors.messages[:base]
 
     assert_raises(ActiveRecord::DeleteRestrictionError) { category.destroy }
+
+    # 子レコードがなければ削除可能
+    order.destroy
+    assert_difference 'Item.count', -1 do
+      item.reload.destroy
+    end
+    assert_difference 'Category.count', -1 do
+      category.reload.destroy
+    end
   end
 end
